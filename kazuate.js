ef4bc3718fee130c19126e8abebc0463a17d173b
G184242021 <218424@st.takushoku-u.ac.jp>
2022-06-30 15:33:20 +0900

    6/30

--- kazuate.js
+++ kazuate.js
@@ -18,21 +18,21 @@ function hantei() {
     // 課題3-1：ここの判定処理を作成する．
     console.log(kaisu+"回目の予想:"+yoso);
     if(kaisu >= 4) {
-        console.log("答えは"+kotae+"ですよ。もうゲームは終わってま～す。");
+        console.log("答えは"+kotae+"ですよ。もうゲームは終わってます。");
         kaisu = kaisu + 1;
     }
     if(kaisu === 3) {
-        console.log("ざんね～ん！答えは"+kotae+"でした。");
+        console.log("ざんねん！答えは"+kotae+"でした。");
         kaisu = kaisu + 1;
     }
     if(kaisu < 3 && kotae===yoso) {
         console.log("正解です！おめでとう！");
         end();
     }else if(kaisu < 3 && kotae < yoso) {
-        console.log("ざんね～ん！答えはもっと小さいですよ。");
+        console.log("ざんねん！答えはもっと小さいですよ。");
         kaisu = kaisu + 1;
     }else if(kaisu < 3 && kotae > yoso){
-        console.log("ざんね～ん！答えはもっと大きいですよ。");
+        console.log("ざんねん！答えはもっと大きいですよ。");
         kaisu = kaisu + 1;
     }
     function end() {
