f3ed0e9624ce4ee38bb2d466c9a0a0798e0c08ad
G184242021 <218424@st.takushoku-u.ac.jp>
2022-07-23 21:15:32 +0900

    7/23(Sat)-at home

--- gourmet.js
+++ gourmet.js
@@ -4,55 +4,6 @@
 
 /*let c1 = document.querySelector('#print');
 c1.addEventListener('click',genre);*/
-
-
-// genre() の定義
-/*function genre() {
-  let k = document.querySelector('input[word="kaito"]');
-  let word = Math.floor(k.value);
-  let pr = document.querySelector('span#result');
-  let py = document.querySelector('span#word');
-    py.textContent = kaisu + "回目の予想:" + yoso;*/
-
-
-/*
-    if(word === '居酒屋' || word === 'G001') {
-      pr.textContent="居酒屋を検索しました。";
-    }else if(word === 'ダイニングバー・バル || word === G002') {
-      pr.textContent="ダイニングバー・バルを検索しました。";
-    }else if(word ==='創作料理' || word === 'G003') {
-      pr.textContent="創作料理を検索しました。";
-    }else if(word === '和食' || word === 'G004') {
-      pr.textContent="和食を検索しました。";
-    }else if(word === '洋食' || word === 'G005') {
-      pr.textContent="洋食を検索しました。";
-    }else if(word === 'イタリアン・フレンチ' || word === 'G006') {
-      pr.textContent="イタリアン・フレンチを検索しました。";
-    }else if(word === '中華' || word === 'G007') {
-      pr.textContent="中華を検索しました。";
-    }else if(word === '焼き肉・ホルモン' || word === 'G008') {
-      pr.textContent="焼き肉・ホルモンを検索しました。";
-    }else if(word === 'アジア・エスニック料理' || word === 'G009') {
-      pr.textContent="アジア・エスニック料理を検索しました。";
-    }else if(word === '各国料理' || word === 'G010') {
-      pr.textContent="各国料理を検索しました。";
-    }else if(word === 'カラオケ・パーティ' || word === 'G011') {
-      pr.textContent="カラオケ・パーティを検索しました。";
-    }else if(word === 'バー・カクテル' || word === 'G012') {
-      pr.textContent="バー・カクテルを検索しました。";
-    }else if(word === 'ラーメン' || word === 'G013') {
-      pr.textContent="ラーメンを検索しました。";
-    }else if(word === 'カフェ・スイーツ' || word === 'G014') {
-      pr.textContent="カフェ・スイーツを検索しました。";
-    }else if(word === 'その他グルメ' || word === 'G015') {
-      pr.textContent="その他グルメを検索しました。";
-    }else if(word === 'お好み焼き・もんじゃ' || word === 'G016') {
-      pr.textContent="お好み焼き・もんじゃを検索しました。";
-    }else if(word === '韓国料理' || word === 'G017') {
-      pr.textContent="韓国料理を検索しました。";
-    }else {
-      pr.textContent="もう一度入力し直してください";
-      */
     
   /*window.addEventListener('DOMContentLoaded', function() {
     //select要素を取得
@@ -62,23 +13,6 @@ c1.addEventListener('click',genre);*/
     });
   });
 */
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
@@ -100,9 +34,9 @@ function sendRequest() {
 		.then(finish);
 }
 
-
 // 通信が成功した時の処理
 function showResult(resp) {
+
 	// サーバから送られてきたデータを出力
 	let data = resp.data;
 
@@ -111,11 +45,27 @@ function showResult(resp) {
 		data = JSON.parse(data);
 	}
   for (n of data.results.shop) {
-    let a = document.createElement('p');
-  a.textContent = '店舗名:' + n.name;
-  sn.insertAdjacentElement('beforeend',a);
-  }
+    let a = document.createElement('h2');//h2要素を作成
+    let c = document.createElement('p');//p要素を作成
+    let d = document.createElement('p');
+    let e = document.createElement('p');
+    let f = document.createElement('p');
+    let g = document.createElement('p');
 
+    a.textContent = n.name;//a要素のテキストを設定
+    c.textContent = '【アクセス】 : ' + n.access;
+    d.textContent = '【住所】 : ' + n.address;
+    e.textContent = '【営業日・時間・ラストオーダー等】 : ' + n.open;
+    f.textContent = '【予算】 : ' + n.budget.average;
+    g.textContent = '【キャッチ】 : ' + n.genre.catch;
+
+    sn.insertAdjacentElement('beforeend',a);//要素snの子要素の最後にaを追加
+    sn.insertAdjacentElement('beforeend',g);
+    sn.insertAdjacentElement('beforeend',c);
+    sn.insertAdjacentElement('beforeend',d);
+    sn.insertAdjacentElement('beforeend',e);
+    sn.insertAdjacentElement('beforeend',f);
+  }
 
 	
 
@@ -135,5 +85,3 @@ function showError(err) {
 function finish() {
 	console.log('Ajax 通信が終わりました');
 }
-
-
